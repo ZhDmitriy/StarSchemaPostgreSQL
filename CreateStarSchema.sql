4ba6b83b12d85ad0ebbed1d0c7adaf41fc62b5b3
Dmitriy Zhdanov <141666797+ZhDmitriy@users.noreply.github.com>
2025-02-01 15:29:53 +0200

    Update CreateStarSchema.sql

--- CreateStarSchema.sql
+++ CreateStarSchema.sql
@@ -10,7 +10,7 @@ CREATE TABLE customerDim (
 
 CREATE TABLE shopDim (
     shop_id SERIAL PRIMARY KEY,
-    location INTEGER,
+    location VARCHAR(150),
     shipping_type VARCHAR(50), 
    	valid_dttm_from timestamp, 
     valid_dttm_to timestamp
@@ -38,21 +38,13 @@ CREATE TABLE transactionDim (
     valid_dttm_to timestamp
 )
 
-CREATE table dateTransactionDim (
-    date_id SERIAL PRIMARY KEY,
-    day timestamp,
-   	month VARCHAR(50),
-    year VARCHAR(50), 
-    quarter VARCHAR(150)
-)
 
 CREATE table transactionFact (
-    transaction_fact_Id SERIAL PRIMARY KEY,
+    transaction_fact_id SERIAL PRIMARY KEY,
    	fk_customer_id INT REFERENCES customerDim(customer_id),
    	fk_product_id INT REFERENCES productDim(product_id),
    	fk_transaction_id INT REFERENCES transactionDim(transaction_id),
    	fk_shop_id INT REFERENCES shopDim(shop_id),
-   	fk_date_id INT REFERENCES dateTransactionDim(date_id),
    	purchase_amount FLOAT
 )
 
